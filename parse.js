41a1b9f950c83b0b69c867f52478f5d36d889e8e
tans <369600584@qq.com>
2022-03-04 12:46:13 +0800

    -

--- parse.js
+++ parse.js
@@ -14,37 +14,39 @@ module.exports = (htxt, options={})=> {
 	}).filter(function(line){
 		return (line === false || line === undefined)?false: true;
 	})
-
-	// 与外部变量
-
 	Object.assign(options, _options );
 
 	// 第二次遍历把变量替换块标签
-
 	let currentTag = ''
-
 	lines = lines.map(function(line){
-		console.log(line)
 		if(line.startsWith('<') && line.endsWith('>') ){
 			// 兼容标签的属性 <div class="bold"> = bold
 			currentTag = line.split(' ').shift().replace(/[<|>]/g, '');
+			return;
+		}
+
+		line = line.replace(/<([^>]+)>/g, function(tag, name){
+			return options[name]?options[name]:tag;
+		});
+
+		if(line.startsWith('//') && currentTag!='code'){
+			return '';
+		}
+
+		if(line.trim() == '') {
+			if(currentTag){
+				line = `</${currentTag}>`;
+				currentTag = ''
+			}
 		}else{
-			line = line.replace(/<([^>]+)>/g, function(tag, name){
-				return options[name]?options[name]:tag;
-			});
-		}
-
-		if(line.trim() == '' && currentTag) {
-			line = `</${currentTag}>`;
-			currentTag = ''
-		}
-
-
-		if(line.trim() == '-'){
-			line = '\n'
+			line = line.replace(/^\|/, '');
+			line += '<br/>';//默认自动换行
 		}
 		return line;
 	})
 
-	return lines.join('\n');
+	return {
+		html:lines.join(''),
+		options: options
+	}
 }
